3b2e5875cf1c86fc43079f4cbec72f72997ddc63
bocchiarati <88000sofien@gmail.com>
2025-10-29 14:55:52 +0100

    create(DTO) : User
    creation d'un AuthDTO

---
+++ app/DTO/AuthDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DTO;
+
+readonly class AuthDTO {
+    public function __construct(
+        public string $email,
+        public string $nom,
+        public string $prenom,
+    ) {}
+}
