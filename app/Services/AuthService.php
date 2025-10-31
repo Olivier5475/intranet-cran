e5a3bda05cfc47d974f524692fb8723cf5724629
bocchiarati <88000sofien@gmail.com>
2025-10-31 13:10:18 +0100

    fix(service) : User
    getUserByEmail retourne un utilisateur Authenticatable au lieu d'un DTO

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -5,6 +5,8 @@ namespace App\Services;
 use App\DTO\AuthDTO;
 use App\repositories\interfaces\UserRepositoryInterface;
 use App\Services\Interface\DecodageServiceInterface;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 
 class AuthService implements Interface\UserServiceInterface {
@@ -14,39 +16,37 @@ class AuthService implements Interface\UserServiceInterface {
         private string $lang = "fr"
     ) {}
 
+    /**
+     * @throws ConnectionException
+     */
     public function handleUserInDatabase(array $data): void {
         $user = $this->userRepository->getUserByEmail($data['email']);
 
         if (!$user) {
-            $tab_post = array('appel' => "form_rech", 'codelangue' => $this->lang, 'page' => 'listeindividupublic', 'pas_de_session' => 'oui');
-
-            $response = Http::post(config("cas.url"), $tab_post);
-
-            $resultat = $response->body();
-            $tab_annuaire=json_decode($resultat,true);
-
-            $tab_annuaire = $this->decodageService->decode_utf8_recursive($tab_annuaire);
-
-            $isCranMember = 0;
-            foreach ($tab_annuaire as $value) {
-                if (isset($value['email']) && $value['email'] === $data["email"]) {
-                    $isCranMember = 1;
-                    break;
-                }
-            }
-            $data["verified_member_role"] = $isCranMember;
+//            $tab_post = array('appel' => "form_rech", 'codelangue' => $this->lang, 'page' => 'listeindividupublic', 'pas_de_session' => 'oui');
+//
+//            $response = Http::post(config("cas.url"), $tab_post);
+//
+//            $resultat = $response->body();
+//            $tab_annuaire=json_decode($resultat,true);
+//
+//            $tab_annuaire = $this->decodageService->decode_utf8_recursive($tab_annuaire);
+//
+//            $isCranMember = false;
+//            foreach ($tab_annuaire as $value) {
+//                if (isset($value['email']) && $value['email'] === $data["email"]) {
+//                    $isCranMember = true;
+//                    break;
+//                }
+//            }
+//            $data["verified_member_role"] = $isCranMember;
+            $data["verified_member_role"] = true;
             $this->userRepository->createUser($data);
         }
 
     }
 
-    public function getUserByEmail(string $string) : ?AuthDTO {
-        $user = $this->userRepository->getUserByEmail($string);
-        if(!$user) { return null; }
-        return new AuthDTO(
-            email : $user->email,
-            nom : $user->nom,
-            prenom: $user->prenom
-        );
+    public function getUserByEmail(string $string) : ?Authenticatable {
+        return $this->userRepository->getUserByEmail($string);
     }
 }
