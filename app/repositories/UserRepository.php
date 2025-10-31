7f74108672051b568bbfd2ce5abb143dae1a129a
bocchiarati <88000sofien@gmail.com>
2025-10-31 13:04:40 +0100

    fix(repository) : Auth
    si aucun role n'est spécifié, mets automatiquement le role user lors de la creation d'un utilisateur

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -10,7 +10,8 @@ class UserRepository implements UserRepositoryInterface {
         return User::where('email', $email)->first();
     }
     public function createUser(array $data) {
-        if(!$data["role"]) { $data['role'] = 'user'; }
+        if(!isset($data["role"])) { $data['role'] = 'user'; }
+        if(is_null($data['role'])) { $data['role'] = 'user'; }
         User::create($data);
     }
     public function updateUser(int $id, array $data) {
