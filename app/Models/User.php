60de1ee9aec309ad05412c6750818fda33eaf2b4
bocchiarati <88000sofien@gmail.com>
2025-10-31 13:07:09 +0100

    fix(model) : User
    rend l'attribut "role" d'un utilisateur fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
         "email",
         "nom",
         "prenom",
+        "role",
         "verified_member_role",
         "newsletter_role",
         "department_id",
