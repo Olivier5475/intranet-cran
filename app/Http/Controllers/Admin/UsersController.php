aed03ab5b5442a3fe3f6fb5ef00b2535cd9b4448
bocchiarati <88000sofien@gmail.com>
2025-12-15 16:26:54 +0100

    create(controller) admin user

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Exception\FileNotFoundException;
+use App\Exception\PersistenceException;
+use App\Http\Controllers\Controller;
+use App\Services\Interfaces\UserServiceInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Throwable;
+
+class UsersController extends Controller {
+    public function __construct(
+        private readonly UserServiceInterface $usersService,
+    ){}
+
+    public function readAll() {
+        try {
+            return \Inertia\Inertia::render("Admin/Users", [
+               "users" => $this->usersService->readAll()
+            ]);
+        } catch (\Exception) {
+            return redirect()->back()->with("message", "No users found");
+        }
+    }
+
+    public function store(Request $request, $id = null) {
+        // 1. Validation de la requête
+        $validatedData = $request->validate([
+            'nom' => ['required', 'string', 'max:255'],
+            'prenom' => ['required', 'string', 'max:255'],
+            'role' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255'],
+        ]);
+
+        try {
+            if($id) {
+                $this->usersService->update($id, $validatedData);
+                return redirect()->route("home")
+                    ->with("success", "Utilisateur mis à jour avec success");
+            } else {
+                $this->usersService->handleUserInDatabase($validatedData);
+                return redirect()->route("home")
+                    ->with("success", "Utilisateur créé avec success");
+            }
+
+        } catch (BadRequestException $e) {
+            // 400 Bad Request (pour une erreur d'argument si non gérée par la validation)
+            return redirect()->back()->with(['success' => 'Arguments manquants ou invalides.'. $e->getMessage()]);
+
+        } catch (PersistenceException $e) {
+            // 500 Internal Server Error (Erreur BD ou Disque)
+            return redirect()->back()->with(['success' => 'Erreur critique de sauvegarde des données. Veuillez réessayer. '. $e->getMessage()]);
+
+        } catch (Throwable $t) {
+            // Erreur imprévue (la transaction a été rollback dans le service)
+            Log::critical('Fatal Error', [
+                'error' => $t,
+                'id' => $id,
+                'nom' => $validatedData['nom'],
+                'prenom' => $validatedData['prenom'],
+                'role' => $validatedData['role'],
+                'email' => $validatedData['email'],
+            ]);
+            return redirect()->back()->with(['success' => 'Une erreur imprévue est survenue (Code: 500).']);
+        }
+    }
+
+    public function create() {
+        return \Inertia\Inertia::render('Admin/UserForm');
+    }
+    public function update($id) {
+        try {
+            return \Inertia\Inertia::render('Admin/UserForm', [
+                "user" => $this->usersService->readById($id)
+            ]);
+        } catch (BadRequestException $e) {
+            // 400 Bad Request (pour une erreur d'argument si non gérée par la validation)
+            return redirect()->back()->with(['error' => 'Arguments manquants ou invalides.']);
+        } catch (FileNotFoundException $e) {
+            // 404 Not Found
+            return redirect()->back()->with(['error' => 'Le document ou un attachement spécifié est introuvable.']);
+        }
+    }
+
+    public function delete($id) {
+        try {
+            $this->usersService->delete($id);
+            return redirect()->back()->with("success", "Document deleted successfully");
+        } catch (BadRequestException) {
+            // 400 Bad Request (pour une erreur d'argument si non gérée par la validation)
+            return redirect()->back()->with(['error' => 'Arguments manquants ou invalides.']);
+        } catch (PersistenceException) {
+            // 500 Internal Server Error (Erreur BD ou Disque)
+            return redirect()->back()->with(['error' => 'Erreur critique de sauvegarde des données. Veuillez réessayer.']);
+        } catch (Throwable $t) {
+            // Erreur imprévue (la transaction a été rollback dans le service)
+            Log::critical('Unhandled fatal error during document transaction.', ['error' => $t->getMessage(), 'id' => $id]);
+            return redirect()->back()->with(['error' => 'Une erreur imprévue est survenue (Code: 500).']);
+        }
+    }
+}
