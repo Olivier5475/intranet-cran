3fd298300a581c18821bb241ad30394c06cf4482
bocchiarati <88000sofien@gmail.com>
2025-12-15 16:28:06 +0100

    uodate(interface) user service & repo

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -2,10 +2,51 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Exception\PersistenceException;
+use App\Exception\UserNotFoundException;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 interface UserRepositoryInterface {
+
+    /**
+     * @param string $email
+     * @return User|null
+     * @throws UserNotFoundException
+     */
     public function getUserByEmail(string $email) : ?User;
-    public function createUser(array $data);
-    public function updateUser(int $id, array $data);
+
+    /**
+     * @param array $data
+     * @return User
+     * @throws PersistenceException
+     */
+    public function createUser(array $data) : User;
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return void
+     * @throws UserNotFoundException
+     * @throws PersistenceException
+     */
+    public function updateUser(int $id, array $data) : void;
+
+    /**
+     * @return Collection
+     */
+    public function readAll() : Collection;
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    public function getUserById(int $id): User;
+
+    /**
+     * @param int $id
+     * @return void
+     * @throws PersistenceException la suppression n'a pas fonctionné
+     */
+    public function delete(int $id) : void;
 }
--- app/Services/Interfaces/UserServiceInterface.php
+++ app/Services/Interfaces/UserServiceInterface.php
@@ -2,7 +2,16 @@
 
 namespace App\Services\Interfaces;
 
+use App\DTO\AuthDTO;
+use App\Exception\PersistenceException;
+
 interface UserServiceInterface {
+
+    /**
+     * @param array $data
+     * @return void
+     * @throws PersistenceException Erreur de persistance du nouvel utilisateur
+     */
     public function handleUserInDatabase(array $data): void ;
     public function getUserByEmail(string $string);
 
@@ -11,5 +20,14 @@ interface UserServiceInterface {
      * @return int
      */
     public function getCurrentUserId() : int;
+
+    public function update(mixed $id, array $data);
+
+
+    public function readById($id) : AuthDTO ;
+
+    public function delete(int $id);
+
+    public function readAll();
 
 }
