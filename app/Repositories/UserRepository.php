ab768859969ccb5bfcf2bdba54e829cca6fee15f
bocchiarati <88000sofien@gmail.com>
2025-12-15 16:28:45 +0100

    update(service & repo) user
    implémente les modif de l'interface

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,13 +2,21 @@
 
 namespace App\Repositories;
 
+use App\Exception\PersistenceException;
+use App\Exception\UserNotFoundException;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class UserRepository implements Interfaces\UserRepositoryInterface {
     public function getUserByEmail(string $email) : ?User {
-        return User::where('email', $email)->first();
+        $user =  User::where('email', $email)->first();
+        return $user;
     }
-    public function createUser(array $data) {
+
+    /**
+     * @throws \Exception
+     */
+    public function createUser(array $data): User {
         if(empty($data["role"])) { $data['role'] = 'user'; }
         try {
             $user = new User();
@@ -17,11 +25,42 @@ class UserRepository implements Interfaces\UserRepositoryInterface {
             $user->prenom = $data['prenom'];
             $user->role = $data['role'];
             $user->save();
+            return $user;
         } catch (\Exception) {
-            throw new \Exception("Error creating user");
+            throw new PersistenceException("Error creating user");
         }
     }
-    public function updateUser(int $id, array $data) {
-        User::update($data);
+
+    public function updateUser(int $id, array $data): void {
+        $user = User::find($id);
+        if(empty($user)) {
+            throw new UserNotFoundException("Utilisateur introuvable, id : " . $id);
+        }
+        try {
+            $user->nom = $data['nom'];
+            $user->prenom = $data['prenom'];
+            $user->email = $data['email'];
+            $user->role = $data['role'];
+            $user->save();
+        } catch (\Throwable) {
+            throw new PersistenceException("Erreur lors de la mise à jour de l'utilisateur.");
+        }
+    }
+
+    public function readAll(): Collection {
+        return User::all();
+    }
+
+    public function getUserById(int $id) : User {
+        return User::find($id);
+    }
+
+    public function delete(int $id) : void {
+        try {
+            $user = User::find($id);
+            $user->delete();
+        } catch (\Throwable) {
+           throw new PersistenceException();
+        }
     }
 }
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,21 +2,20 @@
 
 namespace App\Services;
 
+use App\DTO\AuthDTO;
+use App\Exception\PersistenceException;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use Exception;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 readonly class AuthService implements Interfaces\UserServiceInterface {
     public function __construct(
         private UserRepositoryInterface $userRepository,
     ) {}
 
-    /**
-     * @throws ConnectionException
-     * @throws Exception
-     */
     public function handleUserInDatabase(array $data): void {
         $user = $this->userRepository->getUserByEmail($data['email']);
 
@@ -24,8 +23,9 @@ readonly class AuthService implements Interfaces\UserServiceInterface {
             $data["verified_member_role"] = true;
             try {
                 $this->userRepository->createUser($data);
-            } catch (Exception $e) {
-                throw new Exception("utilisateur non créer");
+            } catch (PersistenceException $e) {
+                Log::error("Erreur lors de la persistance des données", ["erreur" => $e]);
+                throw $e;
             }
         }
 
@@ -37,5 +37,44 @@ readonly class AuthService implements Interfaces\UserServiceInterface {
 
     public function getCurrentUserId() : int {
         return Auth::id();
+    }
+
+    public function readAll() : array {
+        $users = $this->userRepository->readAll();
+        $res = [];
+        foreach ($users as $user) {
+            $res[] = new AuthDTO(
+                email: $user->email,
+                nom: $user->nom,
+                prenom: $user->prenom,
+                role: $user->role,
+                id : $user->id
+            );
+        }
+        return $res;
+    }
+
+    public function delete(int $id) {
+        try {
+            $this->userRepository->delete($id);
+        } catch (PersistenceException $e) {
+            Log::error($e->getMessage());
+            throw $e;
+        }
+    }
+
+    public function readById($id) : AuthDTO {
+        $user = $this->userRepository->getUserById($id);
+        return new AuthDTO(
+            email: $user->email,
+            nom: $user->nom,
+            prenom: $user->prenom,
+            role: $user->role,
+            id: $user->id,
+        );
+    }
+
+    public function update(mixed $id, array $data) {
+
     }
 }
