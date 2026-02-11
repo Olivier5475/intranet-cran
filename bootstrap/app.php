55c2b6653d1a1ddfe093b358d02be88e3a3af206
bocchiarati <88000sofien@gmail.com>
2026-02-11 14:40:44 +0100

    fix auto logout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
             Authentification::class,
+            HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
