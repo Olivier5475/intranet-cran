46b486ad25add489b5ba72947c739965c94f4512
bocchiarati <88000sofien@gmail.com>
2026-02-12 09:46:07 +0100

    Fix redirection infinie vers logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Services\Interfaces\UserServiceInterface;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller {
     public function logout(Request $request, UserServiceInterface $userService)
