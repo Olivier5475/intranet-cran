fb8f9afffd451741b7d9bd241394e74152fb82a6
bocchiarati <88000sofien@gmail.com>
2026-04-08 12:47:47 +0200

    💄 Update Fil d'Ariane (Breadcrumbs) : précise les dossiers qui sont archivés

--- resources/js/types/folder.ts
+++ resources/js/types/folder.ts
@@ -1,5 +1,7 @@
 export interface Folder {
     id: number;
     name: string;
+    is_archived: boolean; // true si l'élément est archivé, false sinon
+    departements: number[];
     children: Array<Folder> | null;
 }
